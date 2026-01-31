b2ce69dc4b23aef9ff3aeb9a6b0bc5aa723f01a6
WoofZJ <zjwoof@gmail.com>
2026-01-31 17:33:03 +0800

    chore(style): add theme & astro-icon package
    添加了astro-icon库，使用tabler图标；添加了daisyui的主题

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,21 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     },
 
     imageService: "cloudflare"
-  })
+  }),
+
+  integrations: [icon()]
 });
